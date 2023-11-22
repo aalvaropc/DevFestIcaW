0a0762908ceb39950346b6502f156dd18672a314
aalvaropc <aalvaropc@gmail.com>
2023-11-22 00:19:00 -0500

    fix speakers

--- src/components/Acerca/Image/index.jsx
+++ src/components/Acerca/Image/index.jsx
@@ -2,7 +2,7 @@ import "./Image.css";
 
 const Image = () => {
   return (
-    <img src="./img/gdgica.png" className="width__imgg md__imgg" alt="GDG Ica" />
+    <img src="./img/devfest_r.jpg" className="width__imgg md__imgg" alt="GDG Ica" />
   );
 };
 
